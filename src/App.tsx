6f32ee7dbb2f1d8a1225592e185e7be6bf9cd813
Cyronite <prasunsharma@icloud.com>
2025-05-19 16:24:09 -0400

    hero-desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Nav from './components/Nav'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import Circle from './components/BgCircle'
 import './App.css';
 
@@ -12,7 +12,7 @@ function App() {
       <Circle top="-1500px" left="100px" size="2000px" color='#E5E50180'/>
       <Circle top="500px" left="-500px" size="1500px" color='#685FD479'/> 
       <Nav />
-      {/* <Hero /> */}
+      <Hero />
     </>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,60 @@
 import HeroImage from "../assets/HeroImage.png"
+import star from "../assets/star.png"
 export default function hero() {
 
     return (
         <>
-        <div className="flex w-100% h-[100vh] justify-center gap-[100px]">
-            <div id="leftSide" className="flex flex-col ">
-                <div>Title</div>
-                <div>content</div>
+        <div className="w-[100vw]">
+        <div className="flex mx-auto px-[5%] justify-between max-w-[1400px]">
+            <div id="leftside ">
+                <div id="top" className="flex flex-col pt-[15%] w-[650px]">
+                    <div className="modak text-[#333333] text-[70px]">Incubator Hacks</div>
+                    <div className="inter text-[#333333] text-[25px] mb-[20px]">A 2-day long hackathon for complete beginners to explore technology and build a project. Winning project pitches receive technical guidance from mentors to continue developing their ideas after the event.</div>
+                    <a href="#apply" className="bg-[#333333] text-[30px] no-underline w-[180px] px-[30px] py-[10px] inter border-[#333333] text-[white]  rounded-full border-4">Register Now</a>
+                </div>
+                 <div id="bottom" className="flex gap-[50px] mt-[150px] absolute">
+                    <div id="box1" className=" p-[20px] w-[200px] bg-[#ffffff3e] rounded-[20px] backdrop-blur-[3px] shadow-lg border-2 border-[white]">
+                        <img src={star} alt="" />
+                        <div className=" pt-[15px] inter text-[#333333] text-[20px]">Tech conferences where participants build innovative hardware/software projects and present them to judges for prizes.</div>
+                    </div>
+                    <div id="box1" className="mb-[50px] mt-[-50px] p-[20px] w-[200px] bg-[#ffffff3e] rounded-[20px] backdrop-blur-[3px] shadow-lg border-2 border-[white]">
+                        <img src={star} alt="" />
+                        <div className=" pt-[15px] inter text-[#333333] text-[20px]">Tech conferences where participants build innovative hardware/software projects and present them to judges for prizes.</div>
+                    </div>
+                    <div id="box1" className="p-[20px] w-[200px] bg-[#ffffff3e] rounded-[20px] backdrop-blur-[3px] shadow-lg border-2 border-[white]">
+                        <img src={star} alt="" />
+                        <div className=" pt-[15px] inter text-[#333333] text-[20px]">Tech conferences where participants build innovative hardware/software projects and present them to judges for prizes.</div>
+                    </div>
+                </div>
             </div>
+            {/* <div id="leftSide" className="flex flex-col justify-between pt-[5%]">
+                <div id="top" className="flex flex-col gap-[5px]">
+                    <div className="modak text-[#333333] text-[70px]">Incubator Hacks</div>
+                    <div className="inter text-[#333333] text-[25px] mb-[20px]">A 2-day long hackathon for complete beginners to explore technology and build a project. Winning project pitches receive technical guidance from mentors to continue developing their ideas after the event.</div>
+                    <a href="#apply" className="bg-[#333333] text-[30px] no-underline w-[180px] px-[30px] py-[10px] inter border-[#333333] text-[white]  rounded-full border-4">Register Now</a>
+               
+                </div>
+                 <div id="bottom" className="flex gap-[20px]">
+                    <div id="box1" className=" p-[20px] w-[175px] bg-[#ffffff3e] rounded-[20px] backdrop-blur-[3px] shadow-lg border-2 border-[white]">
+                        <img src={star} alt="" />
+                        <div className=" pt-[15px] inter text-[#333333] text-[20px]">Tech conferences where participants build innovative hardware/software projects and present them to judges for prizes.</div>
+                    </div>
+                    <div id="box1" className=" p-[20px] w-[175px] bg-[#ffffff3e] rounded-[20px] backdrop-blur-[3px] shadow-lg border-2 border-[white]">
+                        <img src={star} alt="" />
+                        <div className=" pt-[15px] inter text-[#333333] text-[20px]">Tech conferences where participants build innovative hardware/software projects and present them to judges for prizes.</div>
+                    </div>
+                    <div id="box1" className=" p-[20px] w-[175px] bg-[#ffffff3e] rounded-[20px] backdrop-blur-[3px] shadow-lg border-2 border-[white]">
+                        <img src={star} alt="" />
+                        <div className=" pt-[15px] inter text-[#333333] text-[20px]">Tech conferences where participants build innovative hardware/software projects and present them to judges for prizes.</div>
+                    </div>
+                </div>
+                
+            </div> */}
             <div id="rightSide">
-                <img src={HeroImage} alt="image" className="w-[1500px] " />
+                <img src={HeroImage} alt="image" className="w-[100%]" />
             </div>
         </div>
-            
+        </div> 
         </>
     );
 

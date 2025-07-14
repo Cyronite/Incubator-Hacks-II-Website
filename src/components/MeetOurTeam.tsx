4251cbfc7b83300993e64cf84dd21d3982d1fa16
Cyronite <prasunsharma@icloud.com>
2025-07-14 09:50:00 -0400

    finished v2

--- src/components/MeetOurTeam.tsx
+++ src/components/MeetOurTeam.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {useState } from "react";
 import cartImg from "../assets/cart.png";
 import wheelImg from "../assets/wheel.png";
 import Aanya from "../assets/portraits/Aanya.png";
